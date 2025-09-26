49e3ff28bfb16ba4c34b111bfe9677b720d2e953
ddhn1973 <aqq12257@gmail.com>
2025-09-26 13:10:00 +0900

    20250926 테스트

--- eulyoo2/js/main.js
+++ eulyoo2/js/main.js
@@ -59,26 +59,21 @@ $(document).ready(function(){
     $(window).scroll(function(){ 
         scroll_chk()
     })
-    
 
+    //book swiper 
     const book_swiper = new Swiper('.book .swiper', { /* 팝업을 감싼는 요소의 class명 */
-	slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-	spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
-	breakpoints: {
-		768: {    /* 640px 이상일때 적용 */
-			slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-			spaceBetween: 24,
-		},
-	},
-	//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-		el: '.swiper-pagination', /* 해당 요소의 class명 */
-		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
-	}, // 스위퍼 종료
-});
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            769: {    /* 640px 이상일때 적용 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        navigation: {
+            nextEl: '.book .btn_next',
+            prevEl: '.book .btn_prev',
+        },
+    });//swiper 종료
+
 })//맨끝
