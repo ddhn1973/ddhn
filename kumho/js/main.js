bfddf71257245e4ee205c40f7d29e5df28e2bef4
ddhn1973 <aqq12257@gmail.com>
2025-10-24 17:58:05 +0900

    251024 하루끝

--- kumho/js/main.js
+++ kumho/js/main.js
@@ -9,10 +9,10 @@ $(document).ready(function(){
         autoplay: true, //팝업 자동 실행
         autoplaySpeed: 3000, //팝업이 머무는 시간
         speed: 500, //팝업 전환 속도
-        //fade: true,  //페이드 효과 적용
+        fade: true,  //페이드 효과 적용
         dots: false, //하단 페이지 버튼 (true, false)
         arrows: false,  //다음, 이전팝업 (true, false)
         //pauseOnHover: true, //마우스호버시 일시정지
-        infinite: true, //무한반복 (루프)
+        infinite: true, //무한반복 (loop)
     });
 }) // 끝
