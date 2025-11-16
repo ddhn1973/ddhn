49145cc5c5c9526570ce09b5c33f35b367078868
ddhn1973 <aqq12257@gmail.com>
2025-11-16 21:16:24 +0900

    20251116 집 on

--- S&C/js/main.js
+++ S&C/js/main.js
@@ -33,7 +33,6 @@ $(poObj).each(function(i, e) {
 		},
 	});	
 
-
 		const event_swiper = new Swiper('.event .swiper', { /* 팝업을 감싼는 요소의 class명 */
 		slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
 		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
@@ -65,22 +64,22 @@ $(poObj).each(function(i, e) {
 	
 });
 
-gsap.registerPlugin(ScrollTrigger);
+	gsap.registerPlugin(ScrollTrigger);
 
-const lines = gsap.utils.toArray(".txt-front p");
+	const lines = gsap.utils.toArray(".txt-front p");
 
-lines.forEach((line, i) => {
-  gsap.to(line, {
-    clipPath: "inset(0 0 0 0)", // 완전히 다 보이게
-    ease: "none",
-    scrollTrigger: {
-      trigger: ".split",
-      start: "top top",           // split 시작할 때부터
-      end: "bottom bottom",       // 끝날 때까지 천천히
-      scrub: true,
-    }
-  });
-});
+	lines.forEach((line, i) => {
+	gsap.to(line, {
+		clipPath: "inset(0 0 0 0)", // 완전히 다 보이게
+		ease: "none",
+		scrollTrigger: {
+		trigger: ".split",
+		start: "top top",           // split 시작할 때부터
+		end: "bottom bottom",       // 끝날 때까지 천천히
+		scrub: true,
+		}
+	});
+	});
 
 
 
@@ -163,26 +162,70 @@ lines.forEach((line, i) => {
 	});
 
 
-const menu_swiper = new Swiper('.menu .swiper', { /* 팝업을 감싼는 요소의 class명 */
-    slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-	spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
-	breakpoints: {
-		640: {    /* 640px 이상일때 적용 */
-			slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-			spaceBetween: 20,
+	const menu_swiper = new Swiper('.menu .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {
+			640: {    /* 640px 이상일때 적용 */
+				slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 20,
+			},
 		},
-	},
-	centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
-	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+		centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
-	navigation: {
-		nextEl: '.menu .btn_next',
-		prevEl: '.menu .btn_prev',
-	},
-	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-		el: '.swiper-pagination', /* 해당 요소의 class명 */
-		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
-	},
-});
+		navigation: {
+			nextEl: '.menu .btn_next',
+			prevEl: '.menu .btn_prev',
+		},
+		pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+			el: '.swiper-pagination', /* 해당 요소의 class명 */
+			clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+			type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+		},
+	});
+
+
+
+	const youtube_long_swiper = new Swiper('.long .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            769: {    /* 640px 이상일때 적용 */
+                slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 20,
+            },
+            1025: {    /* 640px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, / 팝업을 화면에 가운데 정렬(가운데 1번이 옴) /
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        // autoplay: {  / 팝업 자동 실행 /
+        //     delay: 2500,
+        //     disableOnInteraction: true,
+        // },
+        navigation: {
+            nextEl: '.long .btn_next',
+            prevEl: '.long .btn_prev',
+        },
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.swiper-pagination', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+        },
+        on: {
+            init: function() {
+                $(".swiper-slide").css('z-index','0')
+                $(".swiper-slide-next").css('z-index','9999')
+            },
+            slideNextTransitionEnd: function() {
+                $(".swiper-slide").css('z-index','0')
+                $(".swiper-slide-next").css('z-index','9999')
+            }
+        },
+    });
+
+
 })//맨끝
